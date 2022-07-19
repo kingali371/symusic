ce0b7d675801e3d7b9ff29aca736a0e243b39803
pqnzbmmwQ92 <104521533+pqnzbmmwQ92@users.noreply.github.com>
2022-07-19 19:03:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ async def start_bot():
     print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
     await user.join_chat("EITHON1")
     await user.join_chat("eithonsupport")
+    await user.join_chat("EITHMU1")
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
