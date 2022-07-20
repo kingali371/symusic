07d24b09b98e418a95867ce805d9400ab276b057
pqnzbmmwQ92 <104521533+pqnzbmmwQ92@users.noreply.github.com>
2022-07-20 22:14:32 +0300

    Update utils.py

--- driver/utils.py
+++ driver/utils.py
@@ -18,7 +18,7 @@ keyboard = InlineKeyboardMarkup(
     [
             [
                 InlineKeyboardButton(text="• الـقـائـمـه♪", callback_data="cbmenu"),
-                InlineKeyboardButton("• الـتـحـديـثـات♪",url=f"https://t.me/FA9SH"),
+                InlineKeyboardButton("• لصنع بوت",url=f"https://t.me/EITHON1/819"),
             ],
             [
                 InlineKeyboardButton(
