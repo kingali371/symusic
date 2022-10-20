b464f1f6d9824ce690b2243b87eb658cc19a847a
pqnzbmmwQ92 <104521533+pqnzbmmwQ92@users.noreply.github.com>
2022-10-20 10:06:31 +0300

    Update rmtrash.py

--- program/rmtrash.py
+++ program/rmtrash.py
@@ -9,7 +9,7 @@ from driver.decorators import sudo_users_only, errors
 downloads = os.path.realpath("program/downloads")
 raw = os.path.realpath(".")
 
-@Client.on_message(command(["rmd", "clear"]) & ~filters.edited)
+@Client.on_message(command(["/rmd", "/clear"]) & ~filters.edited)
 @errors
 @sudo_users_only
 async def clear_downloads(_, message: Message):
@@ -22,7 +22,7 @@ async def clear_downloads(_, message: Message):
         await message.reply_text("❌ **no files downloaded**")
 
         
-@Client.on_message(command(["rmw", "clean"]) & ~filters.edited)
+@Client.on_message(command(["/rmw", "/clean"]) & ~filters.edited)
 @errors
 @sudo_users_only
 async def clear_raw(_, message: Message):
@@ -36,7 +36,7 @@ async def clear_raw(_, message: Message):
         await message.reply_text("❌ **no raw files found**")
 
 
-@Client.on_message(command(["cleanup"]) & ~filters.edited)
+@Client.on_message(command(["/cleanup"]) & ~filters.edited)
 @errors
 @sudo_users_only
 async def cleanup(_, message: Message):
